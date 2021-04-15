1094aea8fe8901909817961d4764a3682ac9aab1
Deepansh Pahwa <deepansh65@gmail.com>
2021-04-15 23:04:09 +0530

    Added try catch and cleaned the code a little

--- file-sorter.py
+++ file-sorter.py
@@ -13,28 +13,30 @@ def store_files(name,number):
     for child in glob2.glob(name+'/*.'+temp):
         data.append(child)
     return data
+try:
+    for i in range(len(list)):
+        child = store_files(path_name,i)
+        if glob2.glob(path_name):
+            #Check if Downloads folder exist
+            if glob2.glob(path_name+'/'+list[i].upper()):
+                #use shutil to move all the pdf files to the pdf folder
+                if glob2.glob(path_name+'**/*.'+list[i]):
+                    for j in child:
+                        shutil.move(j,path_name+'/' + list[i].upper())
+                    print("Files moved to their respective folders")
+                else:
+                    print(f"All files are already arranged in {list[i].upper()} folder")
 
-for i in range(len(list)):
-    child = store_files(path_name,i)
-    if glob2.glob(path_name):
-        #Check if Downloads folder exist
-        if glob2.glob(path_name+'/'+list[i].upper()):
-            #use shutil to move all the pdf files to the pdf folder
-            if glob2.glob(path_name+'**/*.'+list[i]):
+
+            else:
+                path = os.path.join(path_name,list[i].upper())
+                print(f"[+] Folder {list[i].upper()} does not exists, So I'm creating one on your behalf...")
+                time.sleep(2)
+                os.mkdir(path)
                 for j in child:
                     shutil.move(j,path_name+'/' + list[i].upper())
-                print("Files moved to their respective folders")
-            else:
-                print(f"All files are already arranged in {list[i].upper()} folder")
-
-
+                print(f"[+] Folder created and files moved to {list[i].upper()} folder")
         else:
-            path = os.path.join(path_name,list[i].upper())
-            print(f"[+] Folder {list[i].upper()} does not exists, So I'm creating one on your behalf...")
-            time.sleep(2)
-            os.mkdir(path)
-            for j in child:
-                shutil.move(j,path_name+'/' + list[i].upper())
-            print(f"[+] Folder created and files moved to {list[i].upper()} folder")
-    else:
-        print("[+] The path you mentioned does not exists, please enter the path again carefully")
+            print("[+] The path you mentioned does not exists, please enter the path again carefully")
+except error as e:
+    print(e)
